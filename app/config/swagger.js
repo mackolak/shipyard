00c6949d6c57314b284999a00a031c273e282458
Anna Cahojova <anna.cahojova@sk.ibm.com>
2022-08-07 17:05:04 +0200

    Add fastify/swagger plugin and set it up

---
+++ app/config/swagger.js
@@ -0,0 +1,33 @@
+exports.options = {
+  routePrefix: '/doc',
+  swagger: {
+    info: {
+      title: 'Shipyard',
+      description: 'Documentation for shipyard routes',
+      version: '1.0.0',
+    },
+    externalDocs: {
+      url: 'https://swagger.io',
+      description: 'Find more info here',
+    },
+    host: 'localhost',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+  },
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next();
+    },
+    preHandler: function (request, reply, next) {
+      next();
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  exposeRoute: true,
+};
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const server = require('./app');
+const swaggerConfig = require('./app/config/swagger');
 
 const start = async () => {
   try {
     const fastify = server();
+    fastify.register(require('@fastify/swagger'), swaggerConfig.options);
     await fastify.listen({ port: 4200 }, (err, address) => {
       if (err) {
         server.log.error(err);
         process.exit(1);
       }
     });
+    // fastify/swagger is not loading the routes into documentation - TBD
+    // fastify.swagger();
   } catch (err) {
     console.error(err);
     process.exit(1);
